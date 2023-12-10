df45aae1dba6fcc2fac50b48acc7637a1bd30bb3
Lindos-Mahadi <lindosmahadi@gmail.com>
2023-12-10 15:11:29 +0600

    Display Rezor table Data on Jquery DataTable using download file

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,22 @@
+﻿using JqueryDataTableProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace JqueryDataTableProject.Controllers
+{
+    public class DemoController : Controller
+    {
+        private readonly AppDbContext appDbContext;
+
+        public DemoController(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var list = appDbContext.TblContacts.ToList();
+            return View(list);
+        }
+    }
+}
